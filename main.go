683abd764731c46578da9290f1eb113c96dd4d34
Valdo Mpinga <valdompinga2@gmail.com>
2023-12-12 15:55:24 +0000

    data ty[es

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go conference"
+	const conferenceTickets uint8 = 50
+	var remainingTickets uint8 = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T\n", conferenceTickets, remainingTickets)
+	fmt.Println()
 
 	fmt.Printf("Welcome to %v our booking app\n", conferenceName)
-	fmt.Printf("We have total of %v tickets and %v are availabe.\n", conferenceTickets,remainingTickets)
+	fmt.Printf("We have total of %v tickets and %v are availabe.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var username string
+	var userTickets int
+
+	username = "John Doe Da Goat"
+	userTickets = 2
+
+	fmt.Println()
+
+	fmt.Printf("User %v booked %v tickets\n", username, userTickets)
 }
