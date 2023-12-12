d16c89e00604ecabefb9a627c30ecdd72684497c
Valdo Mpinga <valdompinga2@gmail.com>
2023-12-12 16:19:06 +0000

    user input and pointers

--- main.go
+++ main.go
@@ -3,24 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName string = "Go conference"
+	conferenceName := "Go conference"
 	const conferenceTickets uint8 = 50
 	var remainingTickets uint8 = 50
-
-	fmt.Printf("conferenceTickets is %T, remainingTickets is %T\n", conferenceTickets, remainingTickets)
-	fmt.Println()
 
 	fmt.Printf("Welcome to %v our booking app\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are availabe.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var username string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-
-	username = "John Doe Da Goat"
-	userTickets = 2
 
 	fmt.Println()
 
-	fmt.Printf("User %v booked %v tickets\n", username, userTickets)
+	fmt.Println("Enter your first name")
+	fmt.Scan(&firstName)
+
+	fmt.Println()
+
+	fmt.Println("Enter your last name")
+	fmt.Scan(&lastName)
+
+	fmt.Println()
+
+	fmt.Println("Enter your email")
+	fmt.Scan(&email)
+
+	fmt.Println()
+
+	fmt.Println("Enter the amount of tickets")
+	fmt.Scan(&userTickets)
+	fmt.Println()
+
+	fmt.Printf("Thank you %v %v for bookin %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
